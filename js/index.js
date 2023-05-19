ccd263d2b7f59095b38a6c727c02eb3954c9b77c
BojanVukobrat <bojanvukobrat@tutanota.com>
2023-05-19 02:03:51 +0200

    Izmena forme za dodavanje igraca

--- js/index.js
+++ js/index.js
@@ -39,6 +39,7 @@ function getAllPlayers(id){
             console.log(response);
             const data = JSON.parse(response);
             clearPlayers();
+            addFormToContainer(id);
             for (let i = 0; i < data.length; i++) {
                 const id = data[i].id;
                 const name = data[i].name;
@@ -108,18 +109,19 @@ function addPlayerToBottom(id, name, position) {
     clubElement.appendChild(pointsElement);
   }
 
-  function addPlayer(){
-    //
+  function addPlayer(id){
+    const name = document.getElementById('name').value;
+    const position = document.getElementById('position').value;
     $.ajax({
         url: 'handlers/addPlayer.php',
         type: 'post',
         data: {
-            "name": "Test Test",
-            "position": 'attack',
-            "teamId": 1
+            "name": name,
+            "position": position,
+            "teamId": id
         },
         success: function(response){
-
+            window.location.reload();
         },
         error: function(xhr){
             alert("ERROR" + xhr);
@@ -135,13 +137,44 @@ function addPlayerToBottom(id, name, position) {
           id: id,
         },
         success: function (response) {
-          // alert("Sacuvano" + response);
           window.location.reload();
         },
         error: function (xhr) {
           alert("GRESKA" + xhr);
         },
       });
+  }
+  
+  function addFormToContainer(id) {
+    const teamContainer = document.getElementById('team-container-main');
+  
+    const nameLabel = document.createElement('label');
+    nameLabel.setAttribute('for', 'name');
+    nameLabel.textContent = 'Name of the player';
+  
+    const nameInput = document.createElement('input');
+    nameInput.setAttribute('type', 'text');
+    nameInput.setAttribute('id', 'name');
+  
+    const positionLabel = document.createElement('label');
+    positionLabel.setAttribute('for', 'position');
+    positionLabel.textContent = 'Position';
+  
+    const positionInput = document.createElement('input');
+    positionInput.setAttribute('type', 'text');
+    positionInput.setAttribute('id', 'position');
+  
+    const addButton = document.createElement('button');
+    addButton.textContent = 'Add Player';
+    addButton.addEventListener('click', function() {
+        addPlayer(id)
+    });
+  
+    teamContainer.appendChild(nameLabel);
+    teamContainer.appendChild(nameInput);
+    teamContainer.appendChild(positionLabel);
+    teamContainer.appendChild(positionInput);
+    teamContainer.appendChild(addButton);
   }
   
   
